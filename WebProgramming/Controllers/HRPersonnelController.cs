7392941265edc17eefa6c0b40acdf301dc1ea9d5
Muhammad Asif <asif4347@icloud.com>
2017-02-01 18:28:50 +0500

    Phase1

---
+++ WebProgramming/Controllers/HRPersonnelController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebProgramming.Controllers
+{
+    public class HRPersonnelController : Controller
+    {
+        // GET: HRPersonnel
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult login()
+        {
+
+
+            return View();
+        }
+        public ActionResult Register()
+        {
+
+
+            return View();
+        }
+    }
+}
